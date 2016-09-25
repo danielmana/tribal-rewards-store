ca628e5a5719109e31e186fd89d463d95b32f1c4
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2016-09-25 21:34:10 +0200

    feat(material): integrate material-ui

--- src/main.js
+++ src/main.js
@@ -16,14 +16,23 @@ import Routes from './common/components/Routes';
 // Base styling
 import './common/base.css';
 
+// Material
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// WORKAROUND Some components use react-tap-event-plugin to listen for touch events
+// https://www.npmjs.com/package/material-ui#react-tap-event-plugin
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 // ID of the DOM element to mount app on
 const DOM_APP_EL_ID = 'app';
 
 // Render the router
 ReactDOM.render((
-  <Router history={browserHistory}>
-    {Routes}
-  </Router>
+  <MuiThemeProvider>
+    <Router history={browserHistory}>
+      {Routes}
+    </Router>
+  </MuiThemeProvider>
 ), document.getElementById(DOM_APP_EL_ID));
 
